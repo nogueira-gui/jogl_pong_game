afec6fbb285a43fc9b4a362ac98ce12fab3ed2c7
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2023-10-04 21:00:51 -0300

    Update MainScene.java
    add blocks and ball

--- src/graphics/MainScene.java
+++ src/graphics/MainScene.java
@@ -3,14 +3,16 @@ package graphics;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLException;
 import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
+import com.jogamp.opengl.glu.gl2.GLUgl2;
+import com.jogamp.opengl.util.gl2.GLUT;
 
 public class MainScene implements GLEventListener {
     private float xMin, xMax, yMin, yMax, zMin, zMax;
 
     private float dir_x, dir_y, dir_z, rot_x, rot_y, rot_z = 0.0f;
-
-
     GLU glu;
 
     @Override
@@ -29,7 +31,7 @@ public class MainScene implements GLEventListener {
         //define a cor da janela (R, G, G, alpha)
         gl.glClearColor(0, 0, 0, 1);
         //limpa a janela com a cor especificada
-        gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity(); //lê a matriz identidade
 
         /*
@@ -38,7 +40,18 @@ public class MainScene implements GLEventListener {
         */
         drawPlayer(gl, dir_x);
 
+        GLUT glut = new GLUT();
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+        drawBall(glut);
 
+        for (float y = 0; y < 3; y++) {
+            for (float i = -5; i < 0; i++) {
+                gl.glColor3f(0.3f * y + 0.2f, 0.15f * y + 0.2f, 0.2f * y + 0.2f);
+                float gap = 1.5f + i * 0.50f;
+                float height = 1.5f - y * 0.15f;
+                drawBlock(gl, gap, height);
+            }
+        }
         gl.glFlush();
     }
 
@@ -72,8 +85,32 @@ public class MainScene implements GLEventListener {
         gl.glEnd();
     }
 
-    private void drawBall(GL2 gl, float x, float y) {
+    private void drawBlock(GL2 gl, float x, float y) {
+        float width = 0.4f;
+        float height = 0.10f;
+
+        // Coordenadas do centro do retângulo
+        float centerX = this.xMin + this.xMax;
+        float centerY = this.yMin + height + 0.10f;
+
+        // Calcula as coordenadas dos vértices
+        float x1 = centerX - width / 2f + x;
+        float x2 = centerX + width / 2f + x;
+        float y1 = centerY - height / 2f + y;
+        float y2 = centerY + height / 2f + y;
+
+        // Desenha o retângulo
+        gl.glBegin(GL2.GL_QUADS);
+        gl.glVertex2f(x1, y1);
+        gl.glVertex2f(x2, y1);
+        gl.glVertex2f(x2, y2);
+        gl.glVertex2f(x1, y2);
+        gl.glEnd();
+    }
+
+    private void drawBall(GLUT glut) {
         //TODO desenhar posicao da bola atualizada
+        glut.glutSolidSphere(0.05, 15, 15);
     }
 
     @Override
