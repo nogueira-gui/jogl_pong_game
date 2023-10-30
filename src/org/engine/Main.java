3482b098bb49d9cff7a9dece2abe3f69aa24b61f
lmello <melloluc02@gmail.com>
2023-10-30 11:44:32 -0300

    :sparkles: feat: Adding submenus

--- src/org/engine/Main.java
+++ src/org/engine/Main.java
@@ -4,6 +4,8 @@ import org.graphics.Renderer;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("jnlp.newt.window.icons", "assets\\heart.png assets\\heart.png");
+
         Renderer.init();
         GameLoop.start();
     }
